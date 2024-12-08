9ff7dc90591b6bdcc0c9451ae03be1d8bc627446
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2024-12-08 18:08:20 +0530

    minor ts error

--- src/components/calendar-view.tsx
+++ src/components/calendar-view.tsx
@@ -12,20 +12,20 @@ import * as React from "react";
 
 import { Card, CardContent } from "@/components/ui/card";
 
-const year = {
-  January: 31,
-  February: 28,
-  March: 31,
-  April: 30,
-  May: 31,
-  June: 30,
-  July: 31,
-  August: 31,
-  September: 30,
-  October: 31,
-  November: 30,
-  December: 31,
-};
+// const year = {
+//   January: 31,
+//   February: 28,
+//   March: 31,
+//   April: 30,
+//   May: 31,
+//   June: 30,
+//   July: 31,
+//   August: 31,
+//   September: 30,
+//   October: 31,
+//   November: 30,
+//   December: 31,
+// };
 
 const daysInCurrentMonth = dayjs().daysInMonth();
 
